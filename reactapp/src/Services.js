66162695fd27e8d96586ae81f18fd0da12150a01
karoldziadkowiec <karol.dziadkkowiec@gmail.com>
2024-02-27 14:25:57 +0100

    navbar and footer init

---
+++ reactapp/src/Services.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const Services = () => {
+
+  return (
+    <div className="App">
+      <h1>Services</h1>
+    </div>
+  );
+}
+
+export default Services;
