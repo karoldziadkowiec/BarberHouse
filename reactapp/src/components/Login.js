c5cbb75e2637cb0a51862f031655d494009805cd
karoldziadkowiec <karol.dziadkkowiec@gmail.com>
2024-03-13 19:23:31 +0100

    Login and Registration page rebuilded

--- reactapp/src/components/Login.js
+++ reactapp/src/components/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Form, Button, Col, Row, Modal } from 'react-bootstrap';
+import { Form, Button, Modal } from 'react-bootstrap';
 import '../App.css';
 import '../styles/Login.css';
 
@@ -10,22 +10,8 @@ const Login = () => {
     password: ''
   });
 
-  const [formData, setFormData] = useState({
-    name: '',
-    surname: '',
-    email: '',
-    password: '',
-    confirmedPassword: '',
-    phone: '',
-    birthday: new Date().toISOString().split('.')[0],
-    address: ''
-  });
-
-  const [termsAccepted, setTermsAccepted] = useState(false);
-  const [showLoginModal, setShowLoginModal] = useState(false);
   const [showModal, setShowModal] = useState(false);
   const [message, setMessage] = useState('');
-  const [successfulMessage, setSuccessfulMessage] = useState('');
   const navigate = useNavigate();
 
   const handleLoginInputChange = (e) => {
@@ -52,8 +38,7 @@ const Login = () => {
         throw new Error('Error: Login failed.');
       }
 
-      setShowLoginModal(true);
-      setSuccessfulMessage('Login successful!');
+      moveToHomePage();
 
     } catch (error) {
       console.error('Error:', error);
@@ -63,165 +48,41 @@ const Login = () => {
   };
 
   const moveToHomePage = () => {
-    navigate('/');
+    navigate('/home');
   };
 
-
-
-  const handleRegistrationInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData(prevState => ({
-      ...prevState,
-      [name]: value
-    }));
-  };
-
-  const handleRegistrationSubmit = async (e) => {
-    e.preventDefault();
-
-    for (const key in formData) {
-      if (formData[key] === '') {
-        setShowModal(true);
-        setMessage('Error: All fields are required.');
-        return;
-      }
-    }
-
-    if (formData.password !== formData.confirmedPassword) {
-      setShowModal(true);
-      setMessage('Error: Passwords do not match.');
-      return;
-    }
-
-    if (!termsAccepted) {
-      setShowModal(true);
-      setMessage('Error: You must confirm the terms and conditions.');
-      return;
-    }
-
-    try {
-      const response = await fetch('https://localhost:7184/api/auth/register', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formData)
-      });
-
-      if (!response.ok) {
-        throw new Error('Error: Registration failed.');
-      }
-
-      setShowModal(true);
-      setMessage('Your account has been successfully registered.');
-      window.location.reload();
-
-    } catch (error) {
-      console.error('Error:', error);
-      window.location.reload();
-    }
+  const moveToRegistrationPage = () => {
+    navigate('/registration');
   };
 
   return (
-    <div className="App">
-      <div className="row">
-        <div className="col-md-6 left-side">
-          <div className="left-container">
-            <Form onSubmit={handleLoginSubmit}>
-              <h3>Sign In</h3>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" name="email" value={loginData.email} onChange={handleLoginInputChange} required />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" name="password" value={loginData.password} onChange={handleLoginInputChange} required />
-              </Form.Group>
-              <div className="d-grid">
-                <Button variant="dark" type="submit">Log In</Button>
-              </div>
-            </Form>
+    <div className="Login">
+      <div className="login-container">
+        <Form onSubmit={handleLoginSubmit}>
+          <h3>Sign In</h3>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label className="white-label">E-mail</Form.Label>
+            <Form.Control type="email" placeholder="Enter e-mail" name="email" value={loginData.email} onChange={handleLoginInputChange} required />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label className="white-label">Password</Form.Label>
+            <Form.Control type="password" placeholder="Enter password" name="password" value={loginData.password} onChange={handleLoginInputChange} required />
+          </Form.Group>
+          <div className="d-grid">
+            <Button variant="warning" type="submit">Log In</Button>
+            <Button variant="secondary" onClick={moveToRegistrationPage}>Register account</Button>
           </div>
-        </div>
+        </Form>
 
-        <div className="col-md-6 right-side">
-          <div className="right-container">
-            <Form onSubmit={handleRegistrationSubmit}>
-              <h3>Sign Up</h3>
-              <Row>
-                <Form.Group as={Col} className="mb-3">
-                  <Form.Label className="white-label">Name</Form.Label>
-                  <Form.Control name="name" size="sm" type="text" placeholder="Enter name" onChange={handleRegistrationInputChange} required />
-                </Form.Group>
-                <Form.Group as={Col} className="mb-3">
-                  <Form.Label className="white-label">Surname</Form.Label>
-                  <Form.Control name="surname" size="sm" type="text" placeholder="Enter surname" onChange={handleRegistrationInputChange} required />
-                </Form.Group>
-              </Row>
-              <Form.Group className="mb-3">
-                <Form.Label className="white-label">E-mail</Form.Label>
-                <Form.Control name="email" size="sm" type="email" placeholder="Enter e-mail" onChange={handleRegistrationInputChange} required />
-              </Form.Group>
-              <Row>
-                <Form.Group as={Col} className="mb-3">
-                  <Form.Label className="white-label">Password</Form.Label>
-                  <Form.Control name="password" size="sm" type="password" placeholder="Enter password" onChange={handleRegistrationInputChange} required />
-                </Form.Group>
-                <Form.Group as={Col} className="mb-3">
-                  <Form.Label className="white-label">Confirm password</Form.Label>
-                  <Form.Control name="confirmedPassword" size="sm" type="password" placeholder="Enter password again" onChange={handleRegistrationInputChange} required />
-                </Form.Group>
-              </Row>
-              <Row>
-                <Form.Group as={Col} className="mb-3">
-                  <Form.Label className="white-label">Phone number</Form.Label>
-                  <Form.Control name="phone" size="sm" type="tel" placeholder="Enter phone number" onChange={handleRegistrationInputChange} required />
-                </Form.Group>
-                <Form.Group as={Col} className="mb-3">
-                  <Form.Label className="white-label">Birthday</Form.Label>
-                  <Form.Control name="birthday" size="sm" type="date" onChange={handleRegistrationInputChange} required />
-                </Form.Group>
-              </Row>
-              <Form.Group className="mb-3">
-                <Form.Label className="white-label">Address</Form.Label>
-                <Form.Control name="address" size="sm" type="text" placeholder="Enter address" onChange={handleRegistrationInputChange} required />
-              </Form.Group>
-              <Form.Check className="white-label" type="switch" id="switcher" label="I confirm the terms and conditions.*" onChange={() => setTermsAccepted(!termsAccepted)} required />
-
-              <Modal show={showLoginModal} onHide={() => setShowLoginModal(false)}>
-                <Modal.Header closeButton>
-                  <Modal.Title>BarberHouse</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>{successfulMessage}</Modal.Body>
-                <Modal.Footer>
-                  <Button variant="success" onClick={moveToHomePage}>
-                    Move to home page
-                  </Button>
-                </Modal.Footer>
-              </Modal>
-              <Modal show={showModal} onHide={() => setShowModal(false)}>
-                <Modal.Header closeButton>
-                  <Modal.Title>BarberHouse</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>{message}</Modal.Body>
-                <Modal.Footer>
-                  <Button variant="secondary" onClick={() => setShowModal(false)}>
-                    Close
-                  </Button>
-                </Modal.Footer>
-              </Modal>
-
-              <Row>
-                <Form.Group as={Col} className="mb-3">
-                  <Button variant="warning" type="submit" className="long-button">Register</Button>
-                </Form.Group>
-                <Form.Group as={Col} className="mb-3">
-                  <Button variant="secondary" type="reset" className="long-button">Clear fields</Button>
-                </Form.Group>
-              </Row>
-            </Form>
-          </div>
-        </div>
+        <Modal show={showModal} onHide={() => setShowModal(false)}>
+          <Modal.Header closeButton>
+            <Modal.Title>BarberHouse</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>{message}</Modal.Body>
+          <Modal.Footer>
+            <Button variant="danger" onClick={() => setShowModal(false)}>Close</Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     </div>
   );
--- reactapp/src/components/Registration.js
+++ reactapp/src/components/Registration.js
@@ -1,11 +1,149 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Form, Button, Col, Row, Modal } from 'react-bootstrap';
 import '../App.css';
+import '../styles/Registration.css';
 
 const Registration = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    surname: '',
+    email: '',
+    password: '',
+    confirmedPassword: '',
+    phone: '',
+    birthday: new Date().toISOString().split('.')[0],
+    address: ''
+  });
+
+  const [termsAccepted, setTermsAccepted] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [message, setMessage] = useState('');
+  const navigate = useNavigate();
+
+  const moveToLoginPage = () => {
+    navigate('/');
+  };
+
+
+  const handleRegistrationInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleRegistrationSubmit = async (e) => {
+    e.preventDefault();
+
+    for (const key in formData) {
+      if (formData[key] === '') {
+        setShowModal(true);
+        setMessage('Error: All fields are required.');
+        return;
+      }
+    }
+
+    if (formData.password !== formData.confirmedPassword) {
+      setShowModal(true);
+      setMessage('Error: Passwords do not match.');
+      return;
+    }
+
+    if (!termsAccepted) {
+      setShowModal(true);
+      setMessage('Error: You must confirm the terms and conditions.');
+      return;
+    }
+
+    try {
+      const response = await fetch('https://localhost:7184/api/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      });
+
+      if (!response.ok) {
+        throw new Error('Error: Registration failed.');
+      }
+
+      setShowModal(true);
+      setMessage('Your account has been successfully registered.');
+      moveToLoginPage();
+
+    } catch (error) {
+      console.error('Error:', error);
+      moveToLoginPage();
+    }
+  };
 
   return (
-    <div className="App">
-      <h1>Registration</h1>
+    <div className="Registration">
+      <div className="registration-container">
+        <Form onSubmit={handleRegistrationSubmit}>
+          <h3>Sign Up</h3>
+          <Row>
+            <Form.Group as={Col} className="mb-3">
+              <Form.Label className="white-label">Name</Form.Label>
+              <Form.Control name="name" size="sm" type="text" placeholder="Enter name" onChange={handleRegistrationInputChange} required />
+            </Form.Group>
+            <Form.Group as={Col} className="mb-3">
+              <Form.Label className="white-label">Surname</Form.Label>
+              <Form.Control name="surname" size="sm" type="text" placeholder="Enter surname" onChange={handleRegistrationInputChange} required />
+            </Form.Group>
+          </Row>
+          <Form.Group className="mb-3">
+            <Form.Label className="white-label">E-mail</Form.Label>
+            <Form.Control name="email" size="sm" type="email" placeholder="Enter e-mail" onChange={handleRegistrationInputChange} required />
+          </Form.Group>
+          <Row>
+            <Form.Group as={Col} className="mb-3">
+              <Form.Label className="white-label">Password</Form.Label>
+              <Form.Control name="password" size="sm" type="password" placeholder="Enter password" onChange={handleRegistrationInputChange} required />
+            </Form.Group>
+            <Form.Group as={Col} className="mb-3">
+              <Form.Label className="white-label">Confirm password</Form.Label>
+              <Form.Control name="confirmedPassword" size="sm" type="password" placeholder="Enter password again" onChange={handleRegistrationInputChange} required />
+            </Form.Group>
+          </Row>
+          <Row>
+            <Form.Group as={Col} className="mb-3">
+              <Form.Label className="white-label">Phone number</Form.Label>
+              <Form.Control name="phone" size="sm" type="tel" placeholder="Enter phone number" onChange={handleRegistrationInputChange} required />
+            </Form.Group>
+            <Form.Group as={Col} className="mb-3">
+              <Form.Label className="white-label">Birthday</Form.Label>
+              <Form.Control name="birthday" size="sm" type="date" onChange={handleRegistrationInputChange} required />
+            </Form.Group>
+          </Row>
+          <Form.Group className="mb-3">
+            <Form.Label className="white-label">Address</Form.Label>
+            <Form.Control name="address" size="sm" type="text" placeholder="Enter address" onChange={handleRegistrationInputChange} required />
+          </Form.Group>
+          <Form.Check className="white-label" type="switch" id="switcher" label="I confirm the terms and conditions.*" onChange={() => setTermsAccepted(!termsAccepted)} required />
+          <Modal show={showModal} onHide={() => setShowModal(false)}>
+            <Modal.Header closeButton>
+              <Modal.Title>BarberHouse</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>{message}</Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={() => setShowModal(false)}>Close</Button>
+            </Modal.Footer>
+          </Modal>
+          <Row>
+            <Button variant="warning" type="submit" className="form-button">Register</Button>
+            <Form.Group as={Col} className="mb-3">
+              <Button variant="danger" type="reset" className="form-button">Clear fields</Button>
+            </Form.Group>
+            <Form.Group as={Col} className="mb-3">
+              <Button variant="secondary" onClick={moveToLoginPage} className="form-button">Back</Button>
+            </Form.Group>
+          </Row>
+        </Form>
+      </div>
     </div>
   );
 }
